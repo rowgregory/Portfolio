ab3d48e4ec3dce6c3d5f1ae2739443afe22ab801
row_gregory <rowgregory@gmail.com>
2018-11-16 20:05:38 -0500

    new star animation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,13 @@ $(document).ready(function(){
     $('.scrollspy').scrollSpy();
   });
 
-  
+
+
+
        
 
-for (var i = 0; i < 600; i++) {
-    var star = '<div class="star" style="animation: twinkle '+((Math.random()*2) + 2)+'s linear '+((Math.random()*5) + .1)+'s infinite; top: '+Math.random()*$(window).height()+'px; left: '+Math.random()*$(window).width()+'px;"></div>';
-    $('body').append(star);
-  }
+// for (var i = 0; i < 300; i++) {
+//     var star = '<div class="star" style="animation: twinkle '+((Math.random()*2) + 2)+'s linear '+((Math.random()*5) + .05)+'s infinite; top: '+Math.random()*$(window).height()+'px; left: '+Math.random()*$(window).width()+'px;"></div>';
+//     $('body').append(star);
+//   }
 
